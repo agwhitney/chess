cf806c49f07a030a520248e213ea7551c0e6c99a
Adam Whitney <agwhitney@partners.org>
2017-06-06 17:24:00 -0400

    Move generation is a little robust and seems to be working better. Introduced a global BS variable to deal with out-of-board steps. TODO includes testing (though it might be best to start ground up somewhere else with that).

--- chess.py
+++ chess.py
@@ -1,22 +1,25 @@
-"""Chess by Adam
-Wherein I try to make a functioning game of chess
+"""Chess v2
+Because things were getting a little out of hand in v1
+Still trying to make Chess
 """
 
 import copy
 import pprint
-pp = pprint.PrettyPrinter(indent=4)     # use pp.pprint instead of print
+pp = pprint.PrettyPrinter(indent=4)
+
+CARDINALS = ['up', 'down', 'left', 'right']
+ORDINALS = ['up-left', 'up-right', 'down-left', 'down-right']
+BS = [(10, 10), None, None]     # Bullshit coordinate - spoof square to catch exceptions with board.step
+# Functionally, if it steps outside the board it will move here. Matches the format of board.square
 
 
 class Piece:
-    """Pieces contain guidelines for legal moves. The engine accepts the move that's desired
-    and processes it, and checks its legality against the object definition (differentials)
-    position = coordinates as list; color = string; symbol = string (character)
-    """
-    def __init__(self, color, name, symbol, hasMoved=False):
+    """Piece object containing properties about the pieces."""
+    def __init__(self, color, name, symbol, count=0):
         self.color = color
         self.name = name
         self.symbol = symbol
-        self.hasMoved = hasMoved
+        self.count = count
 
         if self.color == 'black':
             self.symbol = self.symbol.lower()
@@ -24,244 +27,285 @@ class Piece:
     def __repr__(self):
         return "<{} {} ({})>".format(self.color, self.name, self.symbol)
 
+    def friend_check(self, board, position):
+        """Checks the color of self vs. the color of a piece in (position), using the board.piece_in() function
+        Returns True if the two pieces are FRIENDLY, so if not friend_check() implies an enemy.
+        """
+        try:
+            ufo = board.piece_in(position)
+            friend = True if ufo.color == self.color else False
+        except (AttributeError, KeyError):  # Catch if there's no piece in that position or if it's not in board
+            friend = False
+        return friend
+
 
 class Pawn(Piece):
-    def __init__(self, color):
+    def __init__(self, color, passant=False):
         Piece.__init__(self, color, 'pawn', 'P')
+        self.passant = passant
 
-    def legal_moves(self, difference):
-        if self.color == 'white':
-            if difference[0] == -1:
-                return True
-            elif not self.hasMoved and difference[0] == -2:
-                return True
-            else:
-                return False
-
-        elif self.color == 'black':
-            if difference[0] == 1:
-                return True
-            elif not self.hasMoved and difference[0] == 2:
-                return True
-            else:
-                return False
+    def legal_moves(self, board, start):
+        moves = []
+        forward = 'up' if self.color == 'white' else 'down'
+        # One step
+        step = board.step(start, forward, n=1)
+        if not board.piece_in(step):
+            moves.append(step)
+        # Two step
+        step = board.step(start, forward, n=2)
+        if not board.piece_in(step) and self.count == 0:
+            moves.append(step)
+        # Capture step
+        for direction in ['left', 'right']:
+            step = board.step(start, forward+'-'+direction, n=1)
+            if not self.friend_check(board, step):
+                moves.append(step)
+        # Passant step
+        for direction in ['left', 'right']:
+            step = board.step(start, direction, n=1)
+            side = board.piece_in(step)
+            if side:
+                if side.name == 'pawn' and side.color != self.color:
+                    if side.passant is True and side.count == 1:
+                        moves.append(forward+'-'+direction)     # TODO How to remove the passant-ed pawn?
+                        #  TODO Set pawn's passant flag when it passants
+        return moves
 
 
 class Rook(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'rook', 'R')
 
-    @staticmethod
-    def legal_moves(difference):
-        if 0 in (difference[0], difference[1]):
-            return True
-        else:
-            return False
-      
+    def legal_moves(self, board, start):
+        moves = []
+        for direction in CARDINALS:
+            moves.extend(board.cast_ray(start, direction))
+        print(moves)
+        return moves
+
 
 class Knight(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'knight', 'N')
 
-    @staticmethod
-    def legal_moves(difference):
-        if abs(difference[0]) == 2 and abs(difference[1]) == 1:
-            return True
-        elif abs(difference[0]) == 1 and abs(difference[1]) == 2:
-            return True
-        else:
-            return False
+    def legal_moves(self, board, start):
+        moves = []
+        for direction in ORDINALS:  # Move gathered in two steps: a diagonal step, and then a step with a diagonal comp.
+            first = board.step(start, direction)    # eg an 'up-right' step followed by...
+            move_one = board.step(first, direction.split('-')[0])   # ... an 'up' step...
+            move_two = board.step(first, direction.split('-')[1])   # ... and a 'right' step
+            if not self.friend_check(board, move_one):
+                moves.append(move_one)
+            if not self.friend_check(board, move_two):
+                moves.append(move_two)
+        return moves
 
 
 class Bishop(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'bishop', 'B')
 
-    @staticmethod
-    def legal_moves(difference):
-        if difference[0] == difference[1]:
-            return True
-        else:
-            return False
+    def legal_moves(self, board, start):
+        moves = []
+        for direction in ORDINALS:
+            moves.extend(board.cast_ray(start, direction))
+        return moves
 
 
 class King(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'king', 'K')
 
-    @staticmethod
-    def legal_moves(difference):
-        if abs(difference[0]) <= 1 and abs(difference[1]) <= 1:
-            return True
-        else:
-            return False
+    def legal_moves(self, board, start):
+        # TODO Checking for checking and disallowing self checking
+        moves = []
+        for direction in CARDINALS + ORDINALS:
+            step = board.step(start, direction)
+            if not board.piece_in(step):
+                moves.append(step)
+            elif not self.friend_check(board, start):
+                moves.append(step)
+            else:
+                pass
+        return moves
 
 
 class Queen(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'queen', 'Q')
 
-    @staticmethod
-    def legal_moves(difference):
-        if 0 in (difference[0], difference[1]):
-            return True
-        elif difference[0] == difference[1]:
-            return True
-        else:
-            return False
+    def legal_moves(self, board, start):
+        moves = []
+        for direction in CARDINALS + ORDINALS:
+            moves.extend(board.cast_ray(start, direction))
+        return moves
 
 
 class Board:
-    """Creates the board as a list of strings, a metadata dictionary for each square,
-    and a key to translate algebraic coordinates to cartesian.
-    """
+    """Has info about the board and controls all that occurs atop it."""
     def __init__(self):
         """Creates a board object
         board = 9x9 list (visual; Piece symbols are placed on top)
         square = dictionary: tuple to list(string, string, object/None)
         key = dictionary: string to tuple
         """
-        self.board = []     # Visual presentation
-        self.square = {}    # Maps square (cartesian) to metadata (algCoord, color, piece present)
-        self.key = {}       # Maps algebraic(string) to cartesian coordinates(tuple)
-        
-        # Create the empty checkered board
-        count = 0   # For the checker pattern
+        self.board = []  # Visual presentation
+        self.square = {}  # Maps square (cartesian) to metadata (algCoord, color, piece present)
+        self.key = {}  # Maps algebraic(string) to cartesian coordinates(tuple)
+        self.player = {1: 'white', -1: 'black'}   # Used for turns and checking teams
+
+        self._create_board()
+        self._place_pieces()
+
+    def _create_board(self):
+        """Creates the empty checkered board."""
+        count = 0  # For the checker pattern
         letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-        for letter in letters:     # Denotes row
+        for letter in letters:  # Denotes row
             row = []
-            count += 1      # Checker, not stripe
+            count += 1  # Checker, not stripe
             for number in range(8):  # Denotes column
                 count += 1
-                coord_alg = '{}{}'.format(letter, number+1)
+                coord_alg = '{}{}'.format(letter, number + 1)
                 if count % 2 == 0:
                     color = 'white'
-                    row.append('[ ]')   # White
+                    row.append('[ ]')  # White
                 else:
                     color = 'black'
-                    row.append('[/]')   # Black
-                self.key[coord_alg] = (letters.index(letter)+1, number+1)
-                self.square[(letters.index(letter)+1, number+1)] = [coord_alg, color, None]
+                    row.append('[/]')  # Black
+                self.key[coord_alg] = (letters.index(letter) + 1, number + 1)
+                self.square[(letters.index(letter) + 1, number + 1)] = [coord_alg, color, None]
             self.board.append(row)
 
         # Append the reference row and column
         # Bonus: Now the coordinates fit: (1,1) on the board is actually [1,1]
         self.board.insert(0, [' {} '.format(i) for i in range(9)])
         for row in self.board[1:]:
-            row.insert(0, ' {} '.format(letters[self.board.index(row)-1]))
-      
-    def __repr__(self, square=None):
-        """Return some kind of info (maybe the board, redundant to __str__?). 
-        If square is a string in Key, return info about that square.
-        """
-        if square in self.key:
-            coord = self.key[square]
-            return self.square[coord]
-        else:
-            return self.board
+            row.insert(0, ' {} '.format(letters[self.board.index(row) - 1]))
 
-    def clear_path(self, start, end):
-        """Returns True if the straight path between start and end is free of obstacles
-        (ie self.square(square)[2] is None throughout. Returns False if not clear. Does not
-        check the end coordinate; leaves that to the engine (checking move legality).
-        start and end are tuple coordinates
-        """
-
-
-class Engine:
-    """Brings the Piece and Board classes together to make a chess game!"""
-    def __init__(self):
-        self.board = Board()
-        self.create_pieces()
-
-    def create_pieces(self):
+    def _place_pieces(self):
         # Pawns
         for i in range(1, 9):
-            self.board.square[(2, i)][2] = Pawn('black')
-            self.board.square[(7, i)][2] = Pawn('white')
+            self.square[(2, i)][2] = Pawn('black')
+            self.square[(7, i)][2] = Pawn('white')
         # Rooks
         for col in [1, 8]:
-            self.board.square[(1, col)][2] = Rook('black')
-            self.board.square[(8, col)][2] = Rook('white')
+            self.square[(1, col)][2] = Rook('black')
+            self.square[(8, col)][2] = Rook('white')
         # Knights
         for col in [2, 7]:
-            self.board.square[(1, col)][2] = Knight('black')
-            self.board.square[(8, col)][2] = Knight('white')
+            self.square[(1, col)][2] = Knight('black')
+            self.square[(8, col)][2] = Knight('white')
         # Bishops
         for col in [3, 6]:
-            self.board.square[(1, col)][2] = Bishop('black')
-            self.board.square[(8, col)][2] = Bishop('white')
+            self.square[(1, col)][2] = Bishop('black')
+            self.square[(8, col)][2] = Bishop('white')
         # Queens
-        self.board.square[(1, 4)][2] = Queen('black')
-        self.board.square[(8, 4)][2] = Queen('white')
+        self.square[(1, 4)][2] = Queen('black')
+        self.square[(8, 4)][2] = Queen('white')
         # Kings
-        self.board.square[(1, 5)][2] = King('black')
-        self.board.square[(8, 5)][2] = King('white')
+        self.square[(1, 5)][2] = King('black')
+        self.square[(8, 5)][2] = King('white')
+
+        self.draw_board()
 
     def draw_board(self):
-        """Goes through self.board.square and draws the present pieces on their square, then prints the board.
-        Doesn't change self.board.board - just uses it as a clean sheet each time.
+        """Goes through self.square and draws the present pieces on their square, then prints the board.
+        Doesn't change self.board - just uses it as a clean sheet each time.
         Doesn't return anything - just prints the updated board
         """
-        board = copy.deepcopy(self.board.board)    # Deep copy of the empty board, keeping the original intact
-        for coord in self.board.square:
-            piece = self.board.square[coord][2]
-            if piece is not None:
+        board = copy.deepcopy(self.board)    # Deep copy of the empty board, keeping the original intact
+        for coord in self.square:
+            piece = self.piece_in(coord)
+            if piece:
                 board[coord[0]][coord[1]] = '[{}]'.format(piece.symbol)
         pp.pprint(board)
 
-    def move_piece(self):
-        """Gather as input starting and ending algebraic coordinates.
-        The differential is used with Piece functions to determine if it is legal.
-        If not, or if there is no piece in the starting square, it will start over.
+    def piece_in(self, position):
+        """Returns the piece at a given coordinate (or None)."""
+        try:
+            return self.square[position][2]
+        except KeyError:    # If fed [position] isn't in the board
+            return
+
+    def step(self, start, direction, n=1):
+        """Given an origin square and a direction, gets the square n steps away in that direction
+        and returns the coordinate of that square.
+        origin = tuple(row, col); direction = string
+        returns tuple(coord); or error string
         """
+        move = {
+            'up':         (start[0] - n, start[1]),
+            'down':       (start[0] + n, start[1]),
+            'left':       (start[0], start[1] - n),
+            'right':      (start[0], start[1] + n),
+            'up-left':    (start[0] - n, start[1] - n),
+            'up-right':   (start[0] - n, start[1] + n),
+            'down-left':  (start[0] + n, start[1] - n),
+            'down-right': (start[0] + n, start[1] + n)
+        }
+        if move.get(direction) in self.square:
+            return move.get(direction)
+        else:
+            return BS[0]
+
+    def cast_ray(self, start, direction):
+        """From a starting piece's square, makes a list of open squares in the given direction.
+        If there's a piece there, it will add the legal square only if the colors don't match.
+        Used by Piece class to generate legal moves.
+        """
+        open_squares = []
+        coord = start
+        piece = self.piece_in(start)
         while True:
-            # Gather move coordinates (algebraic) and get the differential
-            try:
-                start, end = input("Move where to where? (separate with space): ").upper().split()
-                start_coord = self.board.key[start]   # tuple here and below
-                end_coord = self.board.key[end]
-                if start_coord == end_coord:
-                    print("The start and end are the same.")
-                    continue
-            except (KeyError, ValueError):
-                print("You messed up!")
+            step_coord = self.step(coord, direction)
+            obstacle = self.piece_in(step_coord)
+            if step_coord == BS[0]:
+                break
+            elif not obstacle:    # If the square in question is empty
+                open_squares.append(step_coord)
+            elif not piece.friend_check(self, step_coord):  # If the obstacle is an enemy
+                open_squares.append(step_coord)
+                break
+            else:       # If an ally
+                break
+            coord = step_coord
+        return open_squares
+
+    def move_piece(self, turn):
+        while True:
+            try:    # Try to gather all the needed variables, and retry on a failure
+                start_alg, end_alg = input("Move where to where? (separate with space): ").upper().split()
+                start = self.key[start_alg]   # tuple here and below
+                end = self.key[end_alg]
+                piece = self.piece_in(start)
+            except (KeyError, ValueError) as err:
+                print(err)
                 continue
-            # Is there even a piece on that square?
-            if self.board.square[start_coord][2] is None:
-                print("No piece in that square")
-                continue    # Start the loop again
-            else:
-                piece = self.board.square[start_coord][2]
-                #
+            if piece.color != self.player[turn]:
+                print("That's not your piece!")
+                continue
 
-            # Check legality in a different function - if it's True, then do the moving bid'nis. False: try again
-            if self.check_move(piece, start_coord, end_coord) is True:
-                print("neat")
-                piece.hasMoved = True
-                self.board.square[end_coord][2] = piece
-                self.board.square[start_coord][2] = None
+            if end in piece.legal_moves(self, start):
+                self.square[end][2] = piece
+                self.square[start][2] = None
+                piece.count += 1
                 self.draw_board()
-                #break
+                break
             else:
-                print("not neat")
-                continue    # Start the loop again
+                print("Not a legal move")
+                continue
 
-    def check_move(self, piece, start_coord, end_coord):
-        """Checks the legality of a given move for a given piece, taking info provided from Engine.move_piece().
-        move = [delta-row, delta-col]; piece = Piece object in starting square
-        """
-        move = [b - a for b, a in zip(end_coord, start_coord)]
-
-        # Check that the move would pass on a blank board
-        if piece.legal_moves(move):
-            return True
+    def play_game(self):
+        p = 1     # 1 or -1 to go with self.player
+        turn_count = {'white': 0, 'black': 0}
+        while True:     # While not game over
+            print("It is {}'s turn".format(self.player[p]))
+            self.move_piece(p)
+            turn_count[self.player[p]] += 1
+            p *= -1
 
 
 if __name__ == '__main__':
-    """'The end of your file has raw logic not wrapped in if __name__ == "__main__":.
-    This means that if someone were to import your file, your program would actually run. This is very bad form.'
-    """
-    chess = Engine()
-    chess.draw_board()
-    chess.move_piece()
+    chess = Board()
+    chess.play_game()
