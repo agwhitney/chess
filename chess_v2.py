931cda2b4bc4ecf5c376a1e49c6ee6acfa151865
Adam Whitney <ag.whitney2@gmail.com>
2017-06-05 20:18:43 -0400

    legal_moves functions (mostly) there and (seemingly) working (a little)

--- chess_v2.py
+++ chess_v2.py
@@ -7,29 +7,8 @@ import copy
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-
-def step(start, direction, n=1):
-    """Given an origin square and a direction, gets the square one step away in that direction
-    and returns the coordinate of that square.
-    origin = tuple(row, col); direction = string
-    returns tuple(coord); or error string
-    """
-    move = {
-        'up':         (start[0] - n, start[1]),
-        'down':       (start[0] + n, start[1]),
-        'left':       (start[0], start[1] - n),
-        'right':      (start[0], start[1] + n),
-        'up-left':    (start[0] - n, start[1] - n),
-        'up-right':   (start[0] - n, start[1] + n),
-        'down-left':  (start[0] + n, start[1] - n),
-        'down-right': (start[0] + n, start[1] + n)
-    }
-    step_coord = move.get(direction, 'nothing')
-    legal_coords = [(row, col) for row in range(1, 9) for col in range(1, 9)]
-    if step_coord in legal_coords:
-        return step_coord
-    else:
-        return "Step leads out of the board."
+CARDINALS = ['up', 'down', 'left', 'right']
+ORDINALS = ['up-left', 'up-right', 'down-left', 'down-right']
 
 
 class Piece:
@@ -48,13 +27,44 @@ class Piece:
 
 
 class Pawn(Piece):
-    def __init__(self, color):
+    def __init__(self, color, passant=False):
         Piece.__init__(self, color, 'pawn', 'P')
+        self.passant = passant
+
+    def legal_moves(self, board, start):
+        moves = []
+        forward = 'up' if self.color == 'white' else 'down'
+        # One step
+        step = board.step(start, forward, n=1)
+        if not board.piece_in(step):
+            moves.append(step)
+        # Two step
+        step = board.step(start, forward, n=2)
+        if not board.piece_in(step) and self.count == 0:
+            moves.append(step)
+        # Capture step
+        for direction in ['left', 'right']:
+            step = board.step(start, forward+direction, n=1)
+            try:
+                if board.piece_in(step) and self.color != board.piece_in(step).color:
+                    moves.append(step)
+            except:     # No piece in the square??
+                pass
+        # Passant step
+        for direction in ['left', 'right']:
+            pass
+        return moves
 
 
 class Rook(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'rook', 'R')
+
+    def legal_moves(self, board, start):
+        moves = []
+        for direction in CARDINALS:
+            moves.extend(board.cast_ray(start, direction))
+        return moves
 
 
 class Knight(Piece):
@@ -66,15 +76,40 @@ class Bishop(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'bishop', 'B')
 
+    def legal_moves(self, board, start):
+        moves = []
+        for direction in ORDINALS:
+            moves.extend(board.cast_ray(start, direction))
+        return moves
+
 
 class King(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'king', 'K')
 
+    def legal_moves(self, board, start):
+        moves = []
+        for direction in CARDINALS + ORDINALS:
+            step = board.step(start, direction)
+            if not board.piece_in(step):
+                moves.append(step)
+            elif board.piece_in(step).color != self.color:
+                moves.append(step)
+            else:
+                pass
+        # castling
+        pass
+
 
 class Queen(Piece):
     def __init__(self, color):
         Piece.__init__(self, color, 'queen', 'Q')
+
+    def legal_moves(self, board, start):
+        moves = []
+        for direction in CARDINALS + ORDINALS:
+            moves.extend(board.cast_ray(start, direction))
+        return moves
 
 
 class Board:
@@ -88,6 +123,7 @@ class Board:
         self.board = []  # Visual presentation
         self.square = {}  # Maps square (cartesian) to metadata (algCoord, color, piece present)
         self.key = {}  # Maps algebraic(string) to cartesian coordinates(tuple)
+
         self._create_board()
 
     def _create_board(self):
@@ -154,15 +190,60 @@ class Board:
 
     def piece_in(self, coord):
         """Returns the piece at a given coordinate (or None)."""
+        # Does None fill the same role as False if I'm checking for a piece there w/ ifs?
         return self.square[coord][2]
 
-    def cast_ray(self, start, end):
-        """From the starting square, casts a 'ray' in the direction of the ending square and returns the coordinate
-        of the first piece that is in the way (which could be the ending piece!). If there are no pieces in the way,
-        it will return None.
+    def step(self, start, direction, n=1):
+        """Given an origin square and a direction, gets the square n steps away in that direction
+        and returns the coordinate of that square.
+        origin = tuple(row, col); direction = string
+        returns tuple(coord); or error string
+        """
+        move = {
+            'up':         (start[0] - n, start[1]),
+            'down':       (start[0] + n, start[1]),
+            'left':       (start[0], start[1] - n),
+            'right':      (start[0], start[1] + n),
+            'up-left':    (start[0] - n, start[1] - n),
+            'up-right':   (start[0] - n, start[1] + n),
+            'down-left':  (start[0] + n, start[1] - n),
+            'down-right': (start[0] + n, start[1] + n)
+        }
+        step_coord = move.get(direction, 'nothing')
+        if step_coord in self.square:
+            return step_coord
+        else:
+            # Some kind of error flag
+            return "Step leads out of the board."
+
+    def cast_ray(self, start, direction):
+        """From a starting piece's square, makes a list of open squares in the given direction.
+        If there's a piece there, it will add the legal square only if the colors don't match.
+        Used by Piece class to generate legal moves.
+        """
+        open_squares = []
+        coord = start
+        while True:
+            try:
+                step_coord = self.step(coord, direction)
+                piece = self.piece_in(start)
+                obstacle = self.piece_in(step_coord)
+                if not obstacle:
+                    open_squares.append(step_coord)
+                elif piece.color != obstacle.color:
+                    open_squares.append(step_coord)
+                    break
+                else:
+                    break
+                coord = step_coord
+            except:
+                break
+        return open_squares
+
+    def get_direction(self, start, end):
+        """From a start and end point, returns the direction from start to end.
         """
         dif = [b - a for b, a in zip(end, start)]    # [delta-row, delta-col]. Needs to be a list to be mutable
-        coord = start
         directions = {
             'up'        : eval('dif[0] < 0 and dif[1] == 0'),
             'down'      : eval('dif[0] > 0 and dif[1] == 0'),
@@ -173,34 +254,33 @@ class Board:
             'down-left' : eval('dif[0] > 0 and dif[1] < 0'),
             'down-right': eval('dif[0] > 0 and dif[1] > 0')
         }
-        for step_dir in directions:
-            if directions[step_dir]:    # If that's the direction
-                while tuple(coord) != end:
-                    step_coord = step(coord, step_dir)
-                    if self.piece_in(step_coord):
-                        return step_coord   # Returns here if there's an obstacle anywhere
-                    coord = step_coord
+        for dyr in directions:
+            if directions[dyr]:
+                return dyr
 
     def move_piece(self):
         while True:
-            try:    # Gather unique start and end and make sure the start has a piece
+            try:    # Try to gather all the needed variables, and retry on a failure
                 start_alg, end_alg = input("Move where to where? (separate with space): ").upper().split()
                 start = self.key[start_alg]   # tuple here and below
                 end = self.key[end_alg]
-                if start == end:
-                    print("Start and End coordinate are the same")
-                    continue
-                if not self.piece_in(start):
-                    print("Starting square is empty")
-                    continue
+                piece = self.piece_in(start)
+                direction = self.get_direction(start, end)
             except (KeyError, ValueError) as err:
                 print(err)
                 continue
 
-            piece = self.piece_in(start)
-            self.square[end][2] = piece
-            self.square[start][2] = None
+            if end in piece.legal_moves(self, start):
+                self.square[end][2] = piece
+                self.square[start][2] = None
+                self.draw_board()
+            else:
+                print("Not a legal move")
+            # break
 
 
 if __name__ == '__main__':
     chess = Board()
+    chess._place_pieces()
+    chess.draw_board()
+    chess.move_piece()
